716c34cf95fc94f69613044f195b83c63a8669a4
ed wassermann <vis0vis@gmail.com>
2020-01-22 21:33:29 +0100

    Extract a few base components for forms

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,17 @@
+<template>
+<div>
+  <button v-on="$listeners" v-bind="$attrs">
+    <slot/>
+  </button>
+</div> 
+</template>
+
+<script>
+  export default {
+    inheritAttrs: false	
+  }
+</script>
+
+<style scoped>
+  
+</style>
